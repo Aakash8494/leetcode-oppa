7112257bd8d0924daaedad1728e2205366f753a0
Aakash Jadhav <72457949+Aakash8494@users.noreply.github.com>
2025-06-25 13:40:42 +0530

    add test case minus for palindrome

--- palindrome-number.ts
+++ palindrome-number.ts
@@ -1,5 +1,11 @@
 function isPalindrome(x: number): boolean {
+    console.log('x.toString(): ', x.toString());
+    console.log('x.toString().split(""): ', x.toString().split(""));
+    console.log('x.toString().split("").reverse(): ', x.toString().split("").reverse());
+    console.log('x.toString().split("").reverse().join(""): ', x.toString().split("").reverse().join(""));
+    console.log('+x.toString().split("").reverse().join(""): ', +x.toString().split("").reverse().join(""));
     return x === +x.toString().split("").reverse().join("");
 }
 
 console.log(isPalindrome(121)); // true
+console.log(isPalindrome(-121)); // true
