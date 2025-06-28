12d85495065865dea076cf473d336a4b015b4e85
Aakash Jadhav <72457949+Aakash8494@users.noreply.github.com>
2025-06-28 12:19:30 +0530

    do local code practice

--- fsd_1_timestamp_prices.js
+++ fsd_1_timestamp_prices.js
@@ -1,52 +1,61 @@
-const Store = function () {
+const Store = function () {// arrow function no no no no .... function bcos of this keyword
+    // constructor() {
     this.store = new Map();
+    // }
 
-    this.add = (timeStamp, price, checkpoint) => {
-        // check if timestamp is already present
-        // then push the new commodity price
-        if (this.store.has(timeStamp)) {
-            const prices = this.store.get(timeStamp);
-            this.store.set(timeStamp, [...prices, price]);
-        }
-        // create a new entry for the timestamp
-        else {
-            this.store.set(timeStamp, [price]);
+    this.add = (timestamp, price, checkpoint) => {
+        if (this.store.has(timestamp)) {
+            const existing_prices = this.store.get(timestamp);
+            this.store.set(timestamp, [...existing_prices, price]);
+        } else {
+            this.store.set(timestamp, [price]);
         }
 
-        // Mark the checkpoint after the last price entry
         if (checkpoint) {
-            const prices = this.store.get(timeStamp);
-            this.store.set(timeStamp, [...prices, checkpoint]);
+            const existing_prices = this.store.get(timestamp);
+            this.store.set(timestamp, [...existing_prices, checkpoint]);
         }
-    };
-
-    this.highestPrice = (timeStamp, checkpoint) => {
-        let prices = this.store.get(timeStamp) ?? [];
-
-        // If checkpoint is passed, get the list till the checkpoint
-        if (checkpoint) {
-            let checkpointIndex = prices.findIndex((e) => e === checkpoint);
-            prices = prices.slice(0, checkpointIndex);
-        }
-
-        return this.getMax(prices);
-    };
-
-    this.getMax = (pricesList) => {
-        // filter non-numeric values (remove checkpoint marks)
-        const prices = pricesList.filter((e) => Number(e));
-        return Math.max(...prices) ?? 0;
     }
-};
+
+
+    this.getHighestPrice = (timestamp, checkpoint) => {
+
+        let prices = [...this.store.get(timestamp)];
+
+
+        if (checkpoint) {
+            let index = prices.findIndex(item => item === checkpoint);
+            prices = prices.splice(0, index);
+        }
+
+        return this.getMaxPrice(prices)
+
+
+    }
+    this.getMaxPrice = (prices) => {
+        console.log('prices: ', prices);
+        // if (prices.length === 0) return 0; 
+        const pricesList = prices.filter((item) => {
+            console.log('Number(item): ', Number(item));
+            return Number(item)// NaN behaves as falsy but not equal to false
+        })
+        return Math.max(...pricesList) ?? 0; // why ?? 
+    }
+
+}
 
 const s = new Store();
-s.add(1, 1);
-s.add(1, 4);
-s.add(1, 2);
-s.add(1, 3, 'a');
-s.add(1, 6);
-s.add(1, 7);
-s.add(1, 8, 'b');
+s.add(1, 1)
+s.add(1, 4)
+s.add(1, 10)
+s.add(1, 2)
+s.add(1, 1, 'a')
+s.add(1, 1)
+s.add(1, 1)
+s.add(1, 9)
+s.add(1, 1, 'b')
 
-console.log(s.highestPrice(1, 'a')); // 4
-console.log(s.highestPrice(1, 'b')); // 8
+console.log(s.getHighestPrice(1, 'a'))
+console.log(s.getHighestPrice(1, 'b'))
+console.log(s.store)
+
